b67373e0f53d9547620a7d2580b2c6c5eb1202f9
C.J. Adams-Collier <cjac@colliertech.org>
2006-10-05 21:44:26 +0000

    2006-10-05  C.J. Collier  <cjcollier@colliertech.org>         * clean.sh         - created - supposedly reverses make.sh         * CodingStandards         - A place for proposed coding standards (patch formats, etc)
    svn path=/trunk/tao/; revision=66321

---
+++ clean.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# Builds the Tao Framework using both Prebuild and autotools 
+
+configure_args=$1
+
+# clean autotools Project Files 
+#other/Prebuild/prebuild /target autotools /file prebuild.xml /clean
+other/Prebuild/prebuild /file prebuild.xml /clean
+
+# Build Solutions Using autotools 
+PACKAGES="Tao.DevIl
+          Tao.GlBindGen
+	  Tao.Ode
+	  Tao.PhysFs
+	  Tao.Sdl
+	  Tao.Lua
+	  Tao.OpenGl
+	  Tao.OpenAl
+	  "
+
+# clean up dist files
+rm -rf dist
+
+ROOTDIR=$PWD
+
+cd $ROOTDIR/src
+
+for thedir in $PACKAGES ;
+do
+  # If the configure.ac file exists, clean
+  if [ -f $thedir/configure.ac ]
+  then
+    pushd $thedir &&
+    autoreconf -i -s &&
+    ./configure $configure_args &&
+    make distclean
+    popd
+  fi
+done
+
+cd $ROOTDIR
+
+# Clean up the left-overs
+for i in \
+    Include.am \
+    autom4te.cache \
+    aclocal.m4 \
+    config.log \
+    config.status
+    config.guess \
+    configure \
+    config.sub \
+    configure.ac \
+    Makefile \
+    Makefile.am \
+    install-sh \
+    missing \
+    '*.pc' \
+    '*.pc.in' ; do
+  find ./ -name $i -exec rm -rf \{\} \;
+done
+
+
