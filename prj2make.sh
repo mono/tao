c8d0ee5726b76c8ec45f6f80a0b86e2a114d7a0c
C.J. Adams-Collier <cjac@colliertech.org>
2006-10-08 21:53:14 +0000

    2006-10-08  C.J. Adams-Collier  <cjcollier@colliertech.org> 	* prj2make.sh 	- added this file.  This will be used by the build verification 	  tests (http://taobvt.colliertech.org) to verify the msvc build 	- note that the build currently fails.  This is better than the 	  "always fail" case that's currently happening, though.
    svn path=/trunk/tao/; revision=66436

---
+++ prj2make.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+./other/Prebuild/prebuild /target vs2003 /file prebuild.xml
+
+PROJECTS="
+    Tao.OpenGl
+    Tao.Glfw
+    Tao.DevIl
+    Tao.Ode
+    Tao.GlGenerator
+    Tao.PhysFs
+    Tao.Platform.Windows
+    Tao.FreeGlut
+    Tao.OpenAl
+    Tao.Lua
+    Tao.Sdl
+    Tao.GlBindGen
+    Tao.Cg"
+
+ROOTDIR=$PWD;
+
+for i in $PROJECTS ; do
+  cd $ROOTDIR/src/$i
+  prj2make $i.csproj && \
+  make || exit 1
+done
+
+cd $ROOTDIR
+
+
